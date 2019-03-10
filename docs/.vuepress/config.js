ca2f5805b9e5c602aaee51f2d7fc4c1a00b91bab
hw_liu <370092760@qq.com>
2019-03-11 00:18:45 +0800

    初始化vuepress项目，初步测试导航栏功能

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  title: 'data Com', // 设置网站标题
+  description: 'Just for fun', //描述
+  dest: './dist',   // 设置输出目录
+  port: 2233, //端口
+  themeConfig: { //主题配置
+    // 添加导航栏
+    nav: [
+      { text: '主页', link: '/' }, // 导航条
+      { text: '组件文档', link: '/baseComponents/' },
+      { text: '知识库', link: '/knowledge/' },
+      { text: 'github',        // 这里是下拉列表展现形式。
+        items: [
+          { text: 'focus-outside', link: 'https://github.com/TaoXuSheng/focus-outside' },
+          { text: 'stylus-converter', link: 'https://github.com/TaoXuSheng/stylus-converter' },
+        ]
+      }
+    ],
+    // 为以下路由添加侧边栏
+    sidebar: {
+      '/frontEnd/': [
+        ['android', '这是安卓'],
+        ['web', '这是web'],
+        ['ios', '这是ios']
+        // 'android', 'web', 'ios'
+      ]
+    }
+  }
+}
